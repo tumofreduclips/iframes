e93300ccb449145c99f6bfd82fd2c9d2c03e5fcd
artak kolyan (aka hanuman) <a.kolyan@gratun.am>
2018-07-02 13:53:33 +0000

    FR_LevelGame and FR_LevelDesign

--- gallery/script.js
+++ gallery/script.js
@@ -1,1 +1,25 @@
- 
+$(document).ready(function(){
+    var slides = slidesData.files;
+    var ind = 0;
+    var slide = slides[ind];
+    var path = slidesData.folder+"/"+slide;
+    $("#img").css("background-image", 'url("'+path+'")');
+
+
+    
+    $("#rarrow").click(function(){
+        ind++;
+        slide = slides[ind];
+        path = slidesData.folder+"/"+slide;
+        $("#img").css("background-image", 'url("'+path+'")');
+    });
+
+
+    $("#larrow").click(function(){
+        ind--;
+        slide = slides[ind];
+        path = slidesData.folder+"/"+slide;
+        $("#img").css("background-image", 'url("'+path+'")');
+    });
+
+});
