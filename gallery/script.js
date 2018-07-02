89aa0866df7ae13cb41a803a90270c0f46495dd3
artak kolyan (aka hanuman) <a.kolyan@gratun.am>
2018-07-02 14:14:30 +0000

    gallery PR1StrangFunctions

--- gallery/script.js
+++ gallery/script.js
@@ -4,22 +4,33 @@ $(document).ready(function(){
     var slide = slides[ind];
     var path = slidesData.folder+"/"+slide;
     $("#img").css("background-image", 'url("'+path+'")');
+    $("#number").html(1+"/"+slides.length);
 
 
     
-    $("#rarrow").click(function(){
-        ind++;
+    $("#rarrow, #img").click(function(){
+        ind++ ;
+        if(ind==slides.length){
+            ind=0;
+        }
         slide = slides[ind];
         path = slidesData.folder+"/"+slide;
         $("#img").css("background-image", 'url("'+path+'")');
+        
+        $("#number").html((ind+1)+"/"+slides.length);
     });
-
 
     $("#larrow").click(function(){
         ind--;
+        if(ind==-1){
+            ind=slides.length-1;
+        }
         slide = slides[ind];
         path = slidesData.folder+"/"+slide;
         $("#img").css("background-image", 'url("'+path+'")');
+
+        $("#number").html((ind+1)+"/"+slides.length);
     });
+
 
 });
