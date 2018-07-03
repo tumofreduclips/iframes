ec294e722cc41734f6bf6009fd6394d6c7d04fed
artak kolyan (aka hanuman) <a.kolyan@gratun.am>
2018-07-03 11:55:33 +0000

    PR4TimingAnimations

---
+++ PR4TimingAnimations/data.js
@@ -0,0 +1,15 @@
+const slidesData = {
+    "folder":"img",
+    "files":[
+        "animation1.gif",
+        "animation2.gif",
+        "animation3.gif",
+        "animation4.gif",
+        "animation5.gif",
+        "animation6.gif",
+        "animation7.gif",
+        "animation8.gif",
+        "animation9.gif",
+        "animation10.gif"
+    ]
+}
