132e15036eb8ded56ba1c3567a1b0fb912501e7d
artak kolyan (aka hanuman) <a.kolyan@gratun.am>
2018-09-06 14:25:52 +0000

    gallery for flowcharts

---
+++ PR21ForFlowcharts/data.js
@@ -0,0 +1,17 @@
+const slidesData = {
+    "folder":"img",
+    "files":[
+        "image7.png",
+        "image8.png",
+        "image16.png",
+        "image22.png",
+        "image26.png",
+        "image30.png",
+        "image35.png",
+        "image38.png",
+        "image45.png",
+        "image48.png",
+        "image54.png",
+        "image55.png"
+    ]
+}
